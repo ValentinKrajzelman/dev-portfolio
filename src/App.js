21eef2b4411df3003a9bb0adcff521254bab5016
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-01-19 20:59:17 -0300

    aca empezamos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter as  Router, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="">
+    holiwi 
     </div>
   );
 }
