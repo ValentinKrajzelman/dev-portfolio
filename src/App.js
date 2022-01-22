4a3502c05d74e1d08e9b35ca0766544800a37894
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-01-21 22:12:34 -0300

    haciendo cosas

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ function App() {
     <Router>
     <div className="bg-orange-200 h-screen md:grid grid-cols-6">
     <Navbar/>
-    <div className="bg-green-200 h-screen col-span-4">wola</div>
-    <div  className="bg-yellow-200 h-screen col-span-1">wola</div>
     </div> 
     </Router>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import {GoPrimitiveDot} from "react-icons/go";
 
 const Navbar = () => {
@@ -7,17 +8,24 @@ const Navbar = () => {
     return (
     <div className="bg-blue-200 h-screen col-span-1">
         <div className="flex flex-col items-center h-full place-content-evenly pb-8">
-            <div className="text-6xl">VK</div>
-            <GoPrimitiveDot className="text-2xl"/>
-            <div className="text-3xl">Home</div>
-            <GoPrimitiveDot className="text-2xl"/>
-            <div className="text-3xl">About</div>
-            <GoPrimitiveDot className="text-2xl"/>
-            <div className="text-3xl">Skills</div>
-            <GoPrimitiveDot className="text-2xl"/>
-            <div className="text-3xl">Projects</div>
-            <GoPrimitiveDot className="text-2xl"/>
-            <div className="text-3xl">Contact</div>
+            <div className="text-6xl">
+            <Link to="/pija">VK</Link>
+            </div>
+            <div className="text-3xl">
+            Home
+            </div>
+            <div className="text-3xl">
+            About
+            </div>
+            <div className="text-3xl">
+            Skills
+            </div>
+            <div className="text-3xl">
+            Projects
+            </div>
+            <div className="text-3xl">
+            Contact
+            </div>
         </div>
     </div>);
 };
