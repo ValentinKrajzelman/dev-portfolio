48a4998d77f53fa90f650e65c26cbf6b7e539c47
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-06 19:43:56 -0300

    wola

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className="">
             <div className="">
-                <div className="">Dejame mostrarte que puedo ofrecer al equipo</div>
+                <div className="">Dejame mostrarte que puedo ofrecer al equipo </div>
                 <div className="">aca va la imagen</div>
             </div>
         </div>
