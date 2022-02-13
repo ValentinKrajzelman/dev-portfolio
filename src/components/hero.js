eabfbea00a712bd5e96209eb6f38981f4b50147c
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-12 23:12:36 -0300

    wola

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
-import opcion1v3proyecto from "./opcion1v3proyecto.png"
+import fotoHero from "./fotoHero.png"
 
 const Hero = () => {
 
     return (
 
-        <div className="h-full grid grid-rows-[20%_20%_60%]">
+        <div className="h-full  grid grid-rows-[20%_20%_60%]">
             <div className="text-5xl row-start-1 flex flex-row items-center place-content-center">VK</div>
             <div className="row-start-2 px-5">Buscas talento? Dejame mostrarte que puedo ofrecer al equipo.</div>
-            <div className=" row-start-3 flex flex-row">
-                <img src={opcion1v3proyecto} />
-                <button><HashLink smooth to='#projects' className="w-auto h-auto">Projectos</HashLink></button>
+            <div className="relative w-screen overflow-hidden row-start-3 bg-[#3B9CB9]">
+                <button><HashLink smooth to='#projects' className="">Projectos</HashLink></button>
+                <img className="absolute bottom-0 left-[50%]" src={fotoHero} />
             </div>
         </div>
 
