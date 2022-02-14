419c960e5b4f97dcfff97b5f06ee09cd9e1ac5f0
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-14 00:23:56 -0300

    a ver vamos con projectos

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,13 +8,13 @@ const Hero = () => {
     return (
 
         <div className="h-full grid grid-rows-[15%_25%_60%]">
-            <div className="text-5xl row-start-1 flex flex-row pt-5 pl-8 items-start place-content-start ">VK</div>
+            <div className="text-5xl row-start-1 flex flex-row pt-5 pl-8 items-start place-content-start text-[#0075FF] font-light"></div>
             <div className="row-start-2 px-10">
-                <div>Hola! Soy Valentin Krajzelman, programador Full-Stack.</div>
-                <div className="">Buscas talento? Dejame mostrarte que puedo ofrecer al equipo.</div>
+                <div className="mb-5 text-xl">Hola! Soy Valentin Krajzelman, <div className="bg-[#845EC2] p-1 inline-block text-white">Programador Full-Stack.</div></div>
+                <div className="text-sm">Buscas talento? Dejame mostrarte que puedo ofrecer al equipo.</div>
             </div>
-            <div className="relative w-screen overflow-hidden row-start-3 bg-[#3B9CB9]">
-                <HashLink className="relative top-[7.5rem] left-10 bg-[#0075FF] rounded-lg text-white text py-2 px-6" smooth to='#projects'>Projectos</HashLink>
+            <div className="relative w-screen overflow-hidden row-start-3 bg-[#3B9CB9] ">
+                <HashLink className="relative top-[7.5rem] left-10 bg-[#0075FF] text-white text py-2 px-6" smooth to='#projects'>Projectos</HashLink>
                 <img className="absolute bottom-0 left-[40%]" src={fotoHero} />
             </div>
         </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,24 +9,20 @@ const Navbar = () => {
 
     let [toggleNavbar, setToggle] = useState('translate-x-[100%]');
 
-    function hamburger() {
-        return (
-            <div></div>);
-    }
 
     return (
         <div>
-            <div className={"fixed text-3xl font-thin z-10 " + toggleNavbar} >
+            <div className={"fixed text-3xl font-light z-10 " + toggleNavbar} >
                 <div className="w-screen h-screen grid grid-rows-[90%_10%]">
                     <div className="flex flex-col place-content-evenly items-center row-start-1 bg-[#B6DEEA]">
                         <div className="flex flex-row">
-                            <HashLink smooth to='#hero' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>VK</HashLink>
+                            <HashLink className="text-[#0075FF] " smooth to='#hero' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Inicio</HashLink>
                             <button onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}><GrClose className="absolute right-10 top-5" /></button>
                         </div>
-                        <HashLink smooth to='#projects' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Projects</HashLink>
-                        <HashLink smooth to='#skills' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Skills</HashLink>
-                        <HashLink smooth to='#about-me' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>About me</HashLink>
-                        <HashLink smooth to='#contact' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Contact</HashLink>
+                        <HashLink className="text-[#0075FF]" smooth to='#projects' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Projects</HashLink>
+                        <HashLink className="text-[#0075FF]" smooth to='#skills' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Skills</HashLink>
+                        <HashLink className="text-[#0075FF]" smooth to='#about-me' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>About me</HashLink>
+                        <HashLink className="text-[#0075FF]" smooth to='#contact' onClick={() => { setToggle(toggleNavbar = 'translate-x-[100%]') }}>Contact</HashLink>
                     </div>
                     <div className="flex flex-row place-content-around items-center row-start-2 bg-[#0075FF] ">
                         <div className="">eng/esp</div>
