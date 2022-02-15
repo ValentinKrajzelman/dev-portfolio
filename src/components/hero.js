1113d63cc0c43b16946d2cb86452b6c1146e3129
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-15 20:32:09 -0300

    wola

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="h-full grid grid-rows-[15%_25%_60%]">
             <div className="text-5xl row-start-1 flex flex-row pt-5 pl-8 items-start place-content-start text-[#0075FF] font-light"></div>
             <div className="row-start-2 px-10">
-                <div className="mb-5 text-xl">Hola! Soy Valentin Krajzelman, <div className="bg-[#845EC2] p-1 inline-block text-white">Programador Full-Stack.</div></div>
+                <div className="mb-5 text-xl">Hola! Soy Valentin Krajzelman, <div className="bg-[#32F1B5] p-1 inline-block ">Programador Full-Stack.</div></div>
                 <div className="text-sm">Buscas talento? Dejame mostrarte que puedo ofrecer al equipo.</div>
             </div>
             <div className="relative w-screen overflow-hidden row-start-3 bg-[#3B9CB9] ">
