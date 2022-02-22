98c55deb59521ffd0dd294abaf7b9e6da3c25550
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-21 22:43:10 -0300

    nose man

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className={"fixed text-3xl font-light z-20 " + toggleNavbar} >
+            <div className={"fixed text-3xl font-light z-20" + toggleNavbar} >
                 <div className="w-screen h-screen grid grid-rows-[90%_10%]">
                     <div className="flex flex-col place-content-evenly items-center row-start-1 bg-[#B6DEEA]">
                         <div className="flex flex-row">
