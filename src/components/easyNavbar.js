aca0c1dd4842fd34f54dbee1a5916084dd9949f7
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-02-26 09:28:53 -0300

    asdf

--- src/components/easyNavbar.js
+++ src/components/easyNavbar.js
@@ -3,7 +3,7 @@ import { HashLink } from 'react-router-hash-link';
 
 const EasyNavbar = () => {
     return (
-        <div className="fixed bottom-0 flex flex-row place-content-evenly items-center bg-[#0075FF] font-sans text-xl underline underline-offset-4 w-screen h-[8vh]">
+        <div className="fixed bottom-0 flex flex-row place-content-evenly items-center bg-[#0075FF] font-sans text-xl underline underline-offset-4 w-screen h-[8vh] ">
             <HashLink smooth to='#skills'>Skills</HashLink>
             <HashLink smooth to='#about-me'>about me</HashLink>
             <HashLink smooth to='#contact'>contact</HashLink>
